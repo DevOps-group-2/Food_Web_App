5083b6c8306acc774f197395ac28451ea54a1022
frejajensen <frejaj9@gmail.com>
2022-09-20 15:59:38 +0200

    test hej freja

--- web/src/App.js
+++ web/src/App.js
@@ -16,7 +16,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    hej freja
+                    hej freja!
                 </a>
                 <button>Login</button>
             </header>
