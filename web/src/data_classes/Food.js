b178f03536444fbc377688893c2437851a5ba4cc
Zahed <zahedm45@gmail.com>
2022-09-20 18:38:56 +0200

    Created a food data class

---
+++ web/src/data_classes/Food.js
@@ -0,0 +1,9 @@
+
+class Food {
+    constructor(id, name, price, description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
