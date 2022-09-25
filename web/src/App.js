d7040c4ce44a30209d4861463984beae46238974
frejajensen <frejaj9@gmail.com>
2022-09-25 08:31:19 +0200

    added few things to drop down

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import appendingOrders from "./adminDashboard/appendingOrders";
 
 function App() {
-    return appendingOrders();
+    return appendingOrders()
 }
 
 export default App;
