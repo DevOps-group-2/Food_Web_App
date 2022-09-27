99f1ea4d58987cb0fa9121ce37df73674a4a05c6
s185736 <65380030+s185736@users.noreply.github.com>
2022-09-27 08:53:01 +0200

    Cleanup..

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import LoginPage from "./client/pages/loginPage/LoginPage.js";
-import Pay from "./client/pages/payment/Pay.js";
+//import LoginPage from "./client/pages/loginPage/LoginPage.js";
+//import Pay from "./client/pages/payment/Pay.js";
 
 import NavigationBar from "./client/components/navigationBar/NavigationBar";
 //import {Route, Switch, Routes, Link, useParams, useNavigate} from "react-router-dom";
 import {Route, Routes} from "react-router-dom";
 import Menu from "./client/pages/menu/Menu";
 import PlaceOrder from "./client/pages/placeOrder/PlaceOrder";
-import AppendingOrders from "./adminDashboard/appendingOrders";
 
 function App() {
     return (
@@ -20,12 +19,11 @@ function App() {
                     <Route path={"/"} element={<h1>home</h1>}/>
                     <Route path={"/menu"} element={<Menu/>}/>
                     <Route path={"/contact"} element={<Contact/>}/>
-                    <Route path={"/login"} element={<LoginPage/>}/>
-                    <Route path={"/order"} element={<Pay/>}/>
+                    <Route path={"/login"} element={ {/*<LoginPage/>*/}}/>
                     <Route path={"/about"} element={<h1>About us</h1>}/>
-                    <Route path={"/payment"} element={<Pay/>}/>
+                    <Route path={"/payment"} element={{/*<Pay/>*/}}/>
                     <Route path={"/placeorder"} element={<PlaceOrder/>}/>
-                    <Route path={"/admin"} element={<AppendingOrders/>}/>
+                    <Route path={"/admin"} element={{/*<AppendingOrders/>*/}}/>
             </Routes>
             </div>
 
