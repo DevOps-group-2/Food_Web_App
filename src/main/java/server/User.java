60fafb692ac73d9d160ef5bbb7ac26d10fc160c3
Zahed <zahedm45@gmail.com>
2022-11-04 20:18:51 +0100

    Database connection is now working

--- src/main/java/server/User.java
+++ src/main/java/server/User.java
@@ -12,7 +12,21 @@ public class User {
     @Column(name = "username")
     private String username;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
 
 
