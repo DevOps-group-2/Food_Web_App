6877ae07ccd7dfc2033aeb9a29de0a107fcfb2d2
Zahed <zahedm45@gmail.com>
2022-11-04 19:23:08 +0100

    Copy and past HibernateController

---
+++ src/main/java/server/HibernateController.java
@@ -0,0 +1,22 @@
+package server;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateController {//Should be a singleton…
+    private final SessionFactory sessionFactory;
+
+    public HibernateController(String dbUrl){
+        Configuration configuration = new Configuration(); //NB org.hibernate.cfg.Configuration
+        configuration.addAnnotatedClass(User.class); //remember to do this for all DB entities
+        configuration.setProperty("hibernate.connection.username", System.getenv("pg"));
+        configuration.setProperty("hibernate.connection.password", System.getenv("de88cff33a1d28c6"));
+        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://" + dbUrl);
+        configuration.setProperty("hibernate.hbm2ddl.auto","update"); //update Schema - don't do this in prod
+        this.sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
---
+++ src/main/java/server/User.java
@@ -0,0 +1,19 @@
+package server;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "DBUSER") //WATCH out  USER is a reserved name!
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+    @Column(name = "username")
+    private String username;
+
+
+}
+
+
+// TODO: Remember Getters and setters as well
