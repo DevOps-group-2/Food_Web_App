8c9147200d9b2d3268213bc85945d797bc26568e
MSAC98 <sammy.chauhan27@gmail.com>
2022-11-05 01:00:02 +0100

    update af customer form

--- web/src/client/pages/customerForm/CustomerForm.js
+++ web/src/client/pages/customerForm/CustomerForm.js
@@ -1,102 +1,106 @@
-// followed this guidehttps://www.geeksforgeeks.org/how-to-create-a-form-in-react/
+// followed this guide https://www.geeksforgeeks.org/how-to-create-a-form-in-react/
 //todo gør det mere interaktivt. https://dev.to/nsebhastian/react-form-real-time-validation-using-state-1eeg
 //todo gør sådan der nogle felter der kan skippes samt andre ikke kan.
 //todo tilføj validations
 
-import {useState} from "react";
+// sidst kigget efter https://www.digitalocean.com/community/tutorials/how-to-build-forms-in-react
+//todo få phone number feltet til at virke så man kan skrive sit telefon nummer måske tillad text istedte for number. 
+
+import {useReducer, useState} from "react";
 //import './CustomerForm.css';
 
-function CustomerForm(){
-    const [date , setDate] = useState('');
-    const [time , setTime] =    useState('');
-    const [name , setName] = useState('');
-    const [phoneNumber , setPhoneNumber] = useState('');
-    const [email , setEmail] = useState('');
-    const [comment , setComment] = useState('');
+const formReducer = (state, e) => {
+    return {
+        ...state,
+        [e.name]: e.value
+    }
+}
 
-    // function to update state of date with
-    // value is entered by the user in the form
-    const handleDateChange =(e)=>{
-        setDate(e.target.value);
-    }
-    // function to update state of time with
-    // value is entered by the user in the form
-    const handleTimeChange =(e)=>{
-        setTime(e.target.value);
-    }
-    // function to update state of name with
-    // value is entered by the user in the form
-    const handleNameChange =(e)=>{
-        setName(e.target.value);
-    }
-    // function to update state of phone number with
-    // value is entered by the user in the form
-    const handlePhoneNumberChange =(e)=>{
-        setPhoneNumber(e.target.value);
-    }
-    // function to update state of email with
-    // value is entered by the user in the form
-    const handleEmailChange =(e)=>{
-        setEmail(e.target.value);
-    }
-    // function to update state of comment with
-    // value is entered by the user in the form
-    const handleCommentChange =(e)=>{
-        setComment(e.target.value);
-    }
+function CustomerForm(){
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+
     // below function will be called when user
     // click on submit button .
     const handleSubmit=(e)=>{
-        // display alert box with user
-        // 'name' and 'email' details for now todo all other details too maybe even a ordernumber.
-        alert(`Your state values: \n 
-            name: ${name} \n 
-            email: ${email}`)
+        e.preventDefault();
+        setSubmitting(true);
+
+        setTimeout(() => {
+            setSubmitting(false);
+            setFormData({
+                reset: true
+            })
+        }, 3000);
     }
+
+    const handleChange = e => {
+        setFormData({
+            name: e.target.name,
+            value: e.target.value,
+        });
+    }
+
     return(
         <div className="App">
             <header className="App-header">
+                {submitting &&
+                    <div>
+                        You are submitting the following:
+                        <ul>
+                            {Object.entries(formData).map(([name, value]) => (
+                                <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+                            ))}
+                        </ul>
+                    </div>
+                }
             <form onSubmit={(e) =>{handleSubmit(e)}}>
                 {/*when user submit the form , handleSubmit() function will be called .*/}
+
+                <fieldset disabled={submitting}>
                 <label >
-                    Date:
-                </label><br/>
-                <input type="text" value={date} required onChange={(e)=> {handleDateChange(e)}} /><br/>
+                    <p>Time:</p>
+
                 { /*when user write in name input box , handleChange() function will be called. */}
+                    <select name="time" onChange={handleChange} value={formData.time || ''}>
+                        <option value="">--Please choose an option--</option>
+                        <option value="15:00">15:00</option>
+                        <option value="16:00">16:00</option>
+                        <option value="17:00">17:00</option>
+                    </select>
+                </label>
 
                 <label >
-                    Time:
-                </label><br/>
-                <input type="text" value={time} required onChange={(e)=> {handleTimeChange(e)}} /><br/>
+                    <p>Name:</p>
+                <input name="name" onChange={handleChange} value={formData.name || ''}/>
                 { /*when user write in name input box , handleChange() function will be called. */}
+                </label>
 
                 <label >
-                    Name:
-                </label><br/>
-                <input type="text" value={name} required onChange={(e)=> {handleNameChange(e)}} /><br/>
-                { /*when user write in name input box , handleChange() function will be called. */}
+                    <p>Phone Number:</p>
+                    <input type="number" name="phoneNumber"  onChange={handleChange} value={formData.number || ''} />
+                    { /*when user write in name input box , handleChange() function will be called. */}
+                </label>
 
                 <label >
-                    Phone Number:
-                </label><br/>
-                <input type="text" value={phoneNumber} required onChange={(e)=> {handlePhoneNumberChange(e)}} /><br/>
-                { /*when user write in name input box , handleChange() function will be called. */}
+                    <p>Email:</p>
+                    <input type="text" name="email"  onChange={handleChange} value={formData.email || ''} />
+                    { /*when user write in name input box , handleChange() function will be called. */}
+                </label>
 
                 <label >
-                    Email:
-                </label><br/>
-                <input type="text" value={email} required onChange={(e)=> {handleEmailChange(e)}} /><br/>
-                { /*when user write in name input box , handleChange() function will be called. */}
+                    <p>Comment:</p>
+                    <input type="text" name="comment"  onChange={handleChange} value={formData.comment || ''} />
+                    { /*when user write in name input box , handleChange() function will be called. */}
+                </label>
+                </fieldset>
 
-                <label >
-                    Comment:
-                </label><br/>
-                <input type="text" value={comment} required onChange={(e)=> {handleCommentChange(e)}} /><br/>
-                { /*when user write in name input box , handleChange() function will be called. */}
-                <input type="submit" value="Submit"/>
-        </form>
+                <button type="submit" disabled={submitting}>Submit</button>
+            </form>
             </header>
         </div>
     )
 }
 export default CustomerForm;
+
+
