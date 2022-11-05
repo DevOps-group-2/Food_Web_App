e502a34302611fba76a6eb6e7c3ac155f2bc9e6f
Zahed <zahedm45@gmail.com>
2022-11-05 17:56:08 +0100

    Cleaned up

--- src/main/java/server/Test.java
+++ src/main/java/server/Test.java
@@ -29,6 +29,4 @@ public class Test {
         session.close();
     }
 
-
-
 }
--- src/main/java/server/User.java
+++ src/main/java/server/User.java
@@ -3,37 +3,6 @@ package server;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-
-/*
-@Entity
-@Table(name = "DBUSER") //WATCH out  USER is a reserved name!
-public class User {
-    @Id
-    @GeneratedValue
-    @Column(name = "id")
-    private int id;
-    @Column(name = "username")
-    private String username;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-}
-*/
-
-
 @Entity
 @Table(name="DBUser") //!!WATCH out  USER is a reserved name!
 @Getter
@@ -43,7 +12,6 @@ public class User {
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class User {
-
     @Id @GeneratedValue
     @Column
     private int id;
