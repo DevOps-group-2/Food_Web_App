b75745894bfa0002b90b6a761aafc1b8bfcff5f5
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2022-11-12 20:49:04 +0100

    trying to get service to work

--- web/src/index.js
+++ web/src/index.js
@@ -6,13 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, HashRouter} from "react-router-dom";
 
 //import {BrowserRouter, HashRouter} from "react-router-dom";
-
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
