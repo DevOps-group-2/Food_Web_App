2e998e71d11f352ced155640909ba2904ba9535e
s185736 <65380030+s185736@users.noreply.github.com>
2022-11-15 09:06:07 +0100

    Small Update.

--- web/src/client/components/placeOrder/basket/Basket.js
+++ web/src/client/components/placeOrder/basket/Basket.js
@@ -25,7 +25,7 @@ function CompletedPage() {
 
 const Basket = (props) => {
 
-    const basketContext = useContext(Context);
+    const cartCtx = useContext(Context);
     const [isSending, setIsSending] = useState(false);
     const [didSubmit, setDidSubmit] = useState(false);
 
@@ -63,11 +63,12 @@ const Basket = (props) => {
         fetch('https://food-webapp.grp2.diplomportal.dk/api/orders', {
             method: 'POST',
             body: JSON.stringify({
-                orderedFoodProducts: basketContext.foodProducts
+                orderedFoodProducts: cartCtx.foodProducts
             })
         });
         setIsSending(false);
         setDidSubmit(true);
+        cartCtx.clearCart();
     };
 
     const basketBoxHandler = (
--- web/src/client/components/placeOrder/basket/Context.js
+++ web/src/client/components/placeOrder/basket/Context.js
@@ -5,7 +5,8 @@ ContextOfBasket = React.createContext({
     foodProducts: [],
     totalAmount: 0,
     addProduct: (item) => {},
-    removeProduct: (id) => {}
+    removeProduct: (id) => {},
+    clearCart : () => {}
 });
 
 export default ContextOfBasket;
