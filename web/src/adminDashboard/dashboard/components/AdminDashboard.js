b9c478b77ffd978c1f631a26d16944bec8854516
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-11-20 05:22:07 +0100

    token work

--- web/src/adminDashboard/dashboard/components/AdminDashboard.js
+++ web/src/adminDashboard/dashboard/components/AdminDashboard.js
@@ -5,7 +5,12 @@ import MainDash from "./mainDash/MainDash";
 import {tokenStore} from "../../../client/stores/TokenStore";
 
 function AdminDashboard(){
-    const isLoggedIn = tokenStore.state
+    let isLoggedIn
+    if (tokenStore?.state === 'logged in') {
+        isLoggedIn = true
+    } else {
+        isLoggedIn = false
+    }
     return(
         <div>
             {isLoggedIn ?   <div className="dashBoard">
