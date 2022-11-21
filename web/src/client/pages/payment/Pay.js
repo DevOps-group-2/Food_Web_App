ea4e473234038dc905be3fa6f3c0de32e5ef04ce
Zahed <zahedm45@gmail.com>
2022-11-21 19:53:51 +0100

    Created the fetching method for Payment

--- web/src/client/pages/payment/Pay.js
+++ web/src/client/pages/payment/Pay.js
@@ -1,99 +1,50 @@
 import React, {Component} from "react";
 import StripeCheckout from "react-stripe-checkout";
-import axios from 'axios'
-import {giraffeStore} from "./GiraffeStore";
-import {observer} from "mobx-react-lite";
 
 
 class Pay extends Component{
     render() {
-        let para = (giraffeName,key) => <li key={key}>{giraffeName}</li>
-
         return (
             <div>
-
-                <ul>
-                    {giraffeStore.giraffes.map(para)}
-                </ul>
-
+                <h1>This is a temporary payment button</h1>
 
                 <StripeCheckout
                     token={handleToken}
                     stripeKey={"pk_test_51Ll7jrJEhBAUpm4shjFR2nezg1jK24pK7XdcovEOeogK4m2HZDdsQITlPVeXromKZoZjHrJO8iWlKhkrDyuOXy8Q00Fv9OdcdE"}
                     billingAddress
                     shippingAddress
-                    amount={10 * 100}
+                    amount={stripePrice}
                     currency={"dkk"}
                     name={"Information"}
                 />
-
-                <h1>Hello there</h1>
-
-
-
             </div>
 
         );
     }
 }
 
-const stripePrice = 10 * 100;
+const stripePrice = 15 * 100;
 const baseUrl = process.env.NODE_ENV === 'development' ?  "http://localhost:8080/":""; //Check if dev environment
-
 
 const handleToken = (token) => {
     // TODO
 
-        console.log("hello there...")
-/*        axios.post('http://localhost:8080/api/stripe/webhook', {
-            amount: 10 * 100,
-            token
-
-        }).then((response) => {
-            alert('payment success')
-
-        }).catch((error) => {
-                alert('Payment failed' + {error})
-            })*/
-
-
-
 
     fetch(baseUrl + "api/stripe/webhook", {
-/*        method: "POST",
+        method: "POST",
         body: token.id,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: "same-origin"*/
+        credentials: "same-origin"
     }).then(function(response) {
-        console.log(response);
+        response.json().then(r => console.log(r))
+        console.log(response)
+        alert('payment success' + {response})
         return response.text()
     }, function(error) {
+        alert('Payment failed' + {error})
         console.log(error.message) ;
     })
 
 }
-
-
-
-/*
-return(
-    <StripeCheckout
-        amount={stripePrice}
-        label="Pay Now"
-        name="Wolf Elite"
-        billingAddress
-        shippingAddress
-        image="https://svgshare.com/i/CUz.svg"
-        description="Your total is" ${price}
-        panelLabel="Pay Now"
-        token={handleToken}
-        stripeKey={publishableKey}
-        currency="USD"
-    />
-)
-*/
 
 
 export default Pay;
