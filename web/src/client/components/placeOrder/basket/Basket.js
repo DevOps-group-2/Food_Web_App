369650f32d17106bcd53168e83f66997c2b926d7
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-11-22 02:46:15 +0100

    order work

--- web/src/client/components/placeOrder/basket/Basket.js
+++ web/src/client/components/placeOrder/basket/Basket.js
@@ -60,14 +60,14 @@ const Basket = (props) => {
 
     const submitOrderHandler = async () => {
         setIsSending(true);
-        console.log(cartCtx.foodProducts.menu)
+        console.log(cartCtx.foodProducts.indexOf(3))
         let response = await fetch('https://food-webapp.grp2.diplomportal.dk/api/orders', {
             "headers": {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             }, method: 'POST',
             body: JSON.stringify({
-                orderedFoodProducts: cartCtx.foodProducts.menu
+                orderedFoodProducts: cartCtx.foodProducts.indexOf(3)
             })
         });
         setIsSending(false);
