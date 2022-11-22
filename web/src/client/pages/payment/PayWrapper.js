9748c44ada0c196a85cce33a838694dfb089c9cc
Zahed <zahedm45@gmail.com>
2022-11-22 20:05:38 +0100

    Cleaned up PayWrapper

--- web/src/client/pages/payment/PayWrapper.js
+++ web/src/client/pages/payment/PayWrapper.js
@@ -1,18 +1,5 @@
-import React, {Component} from "react";
-import StripeCheckout from "react-stripe-checkout";
-import Pay from "./Pay";
-import {Link, Navigate, Route, Routes, useNavigate} from "react-router-dom";
-import NavigationBar from "../../components/navigationBar/NavigationBar";
-import Home from "../homePage/Home";
-import PlaceOrder from "../placeOrder/PlaceOrder";
-import TestForm from "../customerForm/final/TestForm";
-import CustomerForm from "../customerForm/CustomerForm";
-import ContactPage from "../contactPage/ContactPage";
-import AboutUsPage from "../aboutPage/AboutUsPage";
-import LoginPage from "../loginPage/LoginPage";
-import AdminDashboard from "../../../adminDashboard/dashboard/components/AdminDashboard";
-import MainDash from "../../../adminDashboard/dashboard/components/mainDash/MainDash";
-
+import React, from "react";
+import {Navigate} from "react-router-dom";
 
 
 function PayWrapper(){
@@ -28,22 +15,11 @@ function PayWrapper(){
                 <button type="submit">Submit</button>
             </form>
         </div>
-
     );
-
-
 
     function handleSubmit() {
         console.log('You clicked submit.');
         setButtonClicked(true);
-
-        /*    return(
-                <div>
-                    <Routes>
-                        <Route path={"/pay"} element={<Pay/>}/>
-                    </Routes>
-                </div>
-            );*/
     }
 
 }
