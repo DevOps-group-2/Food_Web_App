1064d34a26fe886296ffdd3aea4eb211e31cab6a
frejajensen <frejaj9@gmail.com>
2022-11-30 19:18:44 +0100

    Linket fjernet fra nav bar

--- web/src/client/components/navigationBar/NavigationBar.js
+++ web/src/client/components/navigationBar/NavigationBar.js
@@ -33,9 +33,6 @@ function NavigationBar() {
                 <li>
                     <Link to="/login">Login</Link>
                 </li>
-                <li>
-                    <Link to="/admin">Admin</Link>
-                </li>
             </ul>
             </div>
         </nav>
