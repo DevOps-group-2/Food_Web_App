13261878affbd5f551fed9c02bf10af7b1fccfaf
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2022-11-30 04:08:13 +0100

    integration error fix

--- web/src/client/pages/loginPage/LoginPage.js
+++ web/src/client/pages/loginPage/LoginPage.js
@@ -30,7 +30,7 @@ function LoginPage() {
                     password: pass.value
                 })
             })
-            if (response.ok) {
+            if (response.ok && response.type !== "error") {
                 let token = await response.text()
                 if (token !== '') {
                     console.log(token)
